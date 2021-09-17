96dd74650900fad613fccd579b9675b7a2b1268d
Stefan O'Neil <oneil.stefan@gmail.com>
2021-09-17 16:22:34 +0900

    Added js.

---
+++ Foundations/Javascript Basics/Tasks/Working with variables/myscripts.js
@@ -0,0 +1,13 @@
+// let admin;
+// let names;
+// names = "John";
+// admin = names;
+
+// alert(admin);
+let admin, name; // can declare two variables at once
+
+name = "John";
+
+admin = name;
+
+alert(admin); // "John"
