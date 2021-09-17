36e380e782e18509def10227b2bf3cd1a26ddd51
Stefan O'Neil <oneil.stefan@gmail.com>
2021-09-17 17:50:17 +0900

    Finished first two functions.

---
+++ Foundations/Javascript Basics/Fundamentals Part 3/myscripts.js
@@ -0,0 +1,14 @@
+function favoriteAnimal(animal) {
+  console.log(animal + " is my favorite animal!");
+}
+favoriteAnimal("Pygmy Hippo");
+
+function add7(number) {
+  console.log(number + 7);
+}
+add7(3);
+
+function multiply(number) {
+  console.log(number * number);
+}
+multiply(3, 4);
